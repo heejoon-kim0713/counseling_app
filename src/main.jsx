e542274fb1951ef038b08a502d51fc964f6b0329
heejoon-kim0713 <jesunoaa@gmail.com>
2025-08-13 18:04:06 +0900

    remove React.strictMode

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import AuthProvider from './lib/AuthContext.jsx' // 이 줄 추가
+import AuthProvider from './lib/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* AuthProvider로 App을 감싸줍니다 */}
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
+  // <React.StrictMode>를 제거
+  <AuthProvider>
+    <App />
+  </AuthProvider>
 )
